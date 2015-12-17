43ba376d9751553a22b61ed8af9d4102f6511bcc
Slav <slav@Vyacheslavs-MacBook-Air.local>
2015-12-16 16:19:13 -0800

    Add index file to capture redirects from ftpln.com to fitplan.io

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <META http-equiv="refresh" content="0;URL=http://fitplan.io">
+  </head>
+  <body bgcolor="#ffffff">
+    <center>
+      Redirecting to our website <a href="http://fitplan.io">Fitplan</a>
+    </center>
+</body>
+</html>
